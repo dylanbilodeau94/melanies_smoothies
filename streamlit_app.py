746f1d636a93408f44c1eb772fe283c6377969b3
dylanbilodeau94 <149504770+dylanbilodeau94@users.noreply.github.com>
2024-06-26 16:27:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients:
     ingredient_str = " ".join(ingredients)
     
     for ingredient in ingredients:
-        search_on=pandas_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on=pandas_df.loc[pandas_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(f"{ingredient} Nutrition Information")
